96b23f3fd98f3d9d76522c92a39b603e9c0ec538
henry <henry.chukwu@andela.com>
2017-04-04 14:38:53 +0100

    feature(take-tour): add take tour attribute to users account [Start: #142658229]

--- imports/plugins/included/default-theme/client/index.js
+++ imports/plugins/included/default-theme/client/index.js
@@ -1,5 +1,12 @@
 // Favicons
 import "./favicons";
 
+
+// Styles
+import "./styles/main.less";
+
+// tour
+import "/node_modules/intro.js/introjs.css";
+
 // Scripts
 import "bootstrap/dist/js/npm.js";
