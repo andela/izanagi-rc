bc2684adf17abc2f81566a8048d3f782c8dfa734
rowland henshaw <rowland.henshaw@andela.com>
2017-04-08 15:12:24 +0100

    feature(restful-endpoints): created endpoint that responds to a get call to list all registered shops and added a publication and restful endpoint to view shop counts

---
+++ server/api/custom/routes/shops.js
@@ -0,0 +1,44 @@
+import { Shops } from "/lib/collections";
+import { Counts } from "meteor/tmeasday:publish-counts";
+
+const shopsRoutes = (router, handler) => {
+  router.get("/api/shops", (req, res) => {
+    handler.post(`http://${req.headers.host}/graphql`,
+      { query: `
+          {
+              shops {
+                  _id
+                  name
+                  emails
+                  lastUpdated
+              }
+          }`
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then((result) => {
+          res.status(200).json(result.data);
+        })
+        .catch((error) => {
+          res.status(409).send(error);
+        });
+  });
+};
+export default shopsRoutes;
+
+Meteor.publish("shops-count", function () {
+  Counts.publish(this, "shops-count", Shops.find());
+}, {
+  url: "/api/shops/count"
+});
+JsonRoutes.setResponseHeaders({
+  "Cache-Control": "no-store",
+  "Pragma": "no-cache",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+});
+
